ca8a4f294088a998a354f57f73032d2bba8fb0ba
Ngenge Senior <seniorngenge@gmail.com>
2021-11-06 15:21:35 +0100

    Changed Speaker object

--- lib/sample_data/sample_data.dart
+++ lib/sample_data/sample_data.dart
@@ -1,14 +1,7 @@
 import 'package:smconfapp/data/hall.dart';
 import 'package:smconfapp/data/lat_lon.dart';
 import 'package:smconfapp/data/session.dart';
-import 'package:smconfapp/data/speaker.dart';
 import 'package:smconfapp/data/tag.dart';
-
-var speaker1 = Speaker("1", "https://twitter.com/ngenge_senior", "Ngenge Senior",
-    "SwiftTech", "https://ngengesenior.medium.com", "https://images.unsplash.com/photo-1613186941372-f19da23e373b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
-"Ngenge Senior is a seasoned musician and band from the republic of Congo and will be presenting on some random topic");
-
-List<Speaker> speakers = List<Speaker>.generate(30, (index) => speaker1);
 
 var session = Session(sessionId: "1", title: "Introduction to SM",
     description: "The talk details all about SM and its startups",
